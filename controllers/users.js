1c370e739da16887f11084c007ff9b6ce6cbb137
Рац Никита Дмитриевич <Nikita.Rats@vaz.ru>
2023-07-07 14:50:04 +0400

    fix

--- controllers/users.js
+++ controllers/users.js
@@ -60,13 +60,7 @@ module.exports.login = (req, res, next) => {
   return User.findUserByCredentials(email, password)
     .then((user) => {
       const token = jwt.sign({ _id: user._id }, 'secret', { expiresIn: '7d' });
-      res
-        .cookie('jwt', token, {
-          maxAge: 3600000,
-          httpOnly: true,
-          sameSite: true,
-        })
-        .send({ message: 'Вы успешно авторизовались' });
+      res.send({ token });
     })
     .catch(next);
 };
